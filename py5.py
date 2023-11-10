067e28b38f071e953bce0e7aff458440daa43793
HowToPlayMeow <gamebossmc682@gmail.com>
2023-11-10 10:53:23 +0700

    code v.1 : Meow

---
+++ py5.py
@@ -0,0 +1,9 @@
+
+f_iot = open('iot2.txt','r',encoding='utf-8')
+
+data = f_iot.read()
+
+f_iot.close()
+
+print(data)
+
---
+++ py6.py
@@ -0,0 +1,14 @@
+
+f_iot = open('iot2.txt','r',encoding='utf-8')
+
+data = f_iot.readline()
+print(data)
+data = f_iot.readline()
+print(data)
+data = f_iot.readline()
+print(data)
+data = f_iot.readline()
+print(data)
+
+f_iot.close()
+
